345d6d0de71886b8f1c7e3188db54de8360d62b7
ilyaswilian <74714631+ilyaswilian@users.noreply.github.com>
2021-01-28 19:25:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const { removeBackgroundFromImageFile } = require('remove.bg')
 const { ind } = require('./language')
 const vcard = 'BEGIN:VCARD\n' 
             + 'VERSION:3.0\n' 
-            + 'FN:Affis Admin\n' 
+            + 'FN:IlyasU Admin\n' 
             + 'ORG: Pengembang XBot;\n' 
             + 'TEL;type=CELL;type=VOICE;waid=6282334297175:+62 823-3429-7175\n' 
             + 'END:VCARD' 
@@ -44,7 +44,7 @@ limitawal = '20'
 cr = '*BOT INI SUDAH TERVERIFIKASI*'
 
 /******** OWNER NUMBER**********/
-const ownerNumber = ["62895710073737@s.whatsapp.net","6282334297174@s.whatsapp.net"] 
+const ownerNumber = ["62895704283153@s.whatsapp.net","6282334297174@s.whatsapp.net"] 
 /************************************/
 
        
